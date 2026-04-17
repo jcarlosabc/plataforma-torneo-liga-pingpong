0ca2e52c0df9d5558596c0696b3178759eb4d78b
desarrollofss <desarrollotecnologico@sersocial.org>
2026-04-17 15:40:00 -0500

    fix config

--- database.py
+++ database.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from pathlib import Path
 
-DATABASE_URL = "sqlite:///./ping_pong.db"
+BASE_DIR = Path(__file__).resolve().parent
+DATABASE_URL = f"sqlite:///{BASE_DIR}/ping_pong.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine)
 
